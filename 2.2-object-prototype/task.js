0dedc8198bf671a46ce9a91ba19e6cf24445c529
Yuriy Vyatkin <yuriyvyatkin@yandex.ru>
2021-03-06 15:47:57 +1000

    Finish homework 5

--- 2.2-object-prototype/task.js
+++ 2.2-object-prototype/task.js
@@ -1,11 +1,42 @@
-//String.prototype.isPalindrome - для задачи №1
+'use strict';
+
+String.prototype.isPalindrome = function () {
+    let leftSubstring = this.toLowerCase().match(/[а-я]/g);
+
+    let rightSubstring = leftSubstring
+        .splice(Math.ceil(this.length / 2))
+        .reverse();
+
+    return rightSubstring.every((item, index) => item === leftSubstring[index]);
+};
 
 function getAverageMark(marks) {
-    // код для задачи №2 писать здесь
-    // return averageMark
+    let marksLength = marks.length;
+
+    if (marksLength === 0) {
+        return 0;
+    }
+
+    const reducer = (accumulator, currentValue) => accumulator + currentValue;
+
+    let roundedAverage = Math.round(marks.reduce(reducer) / marksLength);
+
+    return roundedAverage;
 }
 
 function checkBirthday(birthday) {
-    // код для задачи №3 писать здесь
-    // return verdict
+    function UserException(message) {
+        this.message = message;
+        this.name = 'Ошибка ввода данных';
+    }
+
+    if (!birthday) {
+        throw new UserException('Параметр birthday не содержит дату');
+    }
+
+    let diff = Date.now() - new Date(birthday).valueOf();
+
+    let age = diff / (365.25 * 24 * 60 * 60 * 1000);
+
+    return age >= 18;
 }
